ed8df20729c979ae1ac3fd616f2c4ded35b83c08
tobiasras <tobiasras.job@gmail.com>
2026-09-04 11:45:52 +0200

    finished sentiment api

--- sentiment_backend/main.py
+++ sentiment_backend/main.py
@@ -9,6 +9,7 @@ class TextInput(BaseModel):
 app = FastAPI()
 
 afinn_en = Afinn(language="en")
+afinn_da = Afinn(language="da")
 
 @app.get("/")
 async def root():
@@ -16,7 +17,11 @@ async def root():
 
 @app.post("/v1/sentiment")
 def analyze_sentiment(payload: TextInput):
-    score = afinn_en.score(payload.text)
+    score_en = afinn_en.score(payload.text)
+    score_dk = afinn_da.score(payload.text)
+    
+    score = (score_dk + score_en) / 2
+    
     return {"score": score}
 
 if __name__ == "__main__":
