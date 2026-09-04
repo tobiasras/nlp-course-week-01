9f00d502344b4ca60857c91b46bbd6031b00d5f9
tobiasras <tobiasras.job@gmail.com>
2026-09-04 11:25:53 +0200

    mvp

--- sentiment_backend/main.py
+++ sentiment_backend/main.py
@@ -1,21 +1,23 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import uvicorn
-
+from afinn import Afinn
 
 class TextInput(BaseModel):
     text: str
 
 app = FastAPI()
 
+afinn_en = Afinn(language="en")
+
 @app.get("/")
 async def root():
-    return {"message": "app is running good"}
+    return {"message": "app is running good:)"}
 
 @app.post("/v1/sentiment")
-def analyze_sentiment(text: TextInput):
-    print(text)
-    return {"score": 0}
+def analyze_sentiment(payload: TextInput):
+    score = afinn_en.score(payload.text)
+    return {"score": score}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
---
+++ test/sentiment_backend/test_main.py
@@ -0,0 +1,25 @@
+import sys
+from pathlib import Path
+
+from fastapi.testclient import TestClient
+
+sys.path.insert(0, str(Path(__file__).resolve().parents[2] / "sentiment_backend"))
+from main import app
+
+client = TestClient(app)
+
+def test_positive_sentiment():
+    response = client.post("/v1/sentiment", json={"text": "Det var en god lærer."})
+    assert response.status_code == 200
+    assert response.json() == {"score": 3}
+
+def test_negative_sentiment():
+    response = client.post("/v1/sentiment", json={"text": "It was a bad course"})
+    assert response.status_code == 200
+    assert response.json() == {"score": -3}
+
+
+if __name__ == "__main__":
+    test_positive_sentiment()
+    test_negative_sentiment()
+    print("All tests passed!")
